7f02ab54a3d232ee47f008ee88f0f0cacd62b6db
GO0128 <go20040128@gmail.com>
2023-09-29 18:35:25 +0900

    add: 初期バージョンのホームを作成しました。

---
+++ src/components/Home.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+            <header>
+                <div className="p-2 mb-4 head">
+                    <div className="d-flex">
+                        <div className="d-flex flex-grow-1">
+                            <div className="align-self-center">
+                                <div className="top-icon px-2">
+                                    <i class="bi bi-gear"></i>
+                                </div>
+                            </div>
+                            <div className="top-name align-self-center fs-2">
+                                <a className="navbar-brand" href="#">CSS Generater</a>
+                            </div>
+                        </div>
+                        <div className="d-flex">
+                            <div className="align-self-center d-none d-sm-block text-white">
+                                <a className="nav-link" href="#">My Account</a>
+                            </div>
+                            <div className="align-self-center">
+                                <div className="person-icon px-2">
+                                    <i class="bi bi-person-circle"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <div className="container">
+                    <div className="row gy-5 py-3">
+                        <div className="col-sm-4">
+                            <div className="card">
+                                <div className="card-body">
+                                    <h3 className="card-title">liner-gradient</h3>
+                                    <div className="card-icon text-center">
+                                        <i class="bi bi-brush"></i>
+                                    </div>
+                                    <p className="card-text"></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-4">
+                            <div className="card">
+
+                                <div className="card-body">
+                                    <h3 className="card-title">box-shadow</h3>
+                                    <div className="card-icon text-center">
+                                        <i class="bi bi-back"></i>
+                                    </div>
+                                    <p className="card-text"></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-4">
+                            <div className="card">
+
+                                <div className="card-body">
+                                    <h3 className="card-title">flex-container</h3>
+                                    <div className="card-icon text-center">
+                                        <i class="bi bi-grid-3x3-gap"></i>
+                                    </div>
+                                    <p className="card-text"></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-4">
+                            <div className="card">
+
+                                <div className="card-body">
+                                    <h3 className="card-title">talk-to-AI</h3>
+                                    <div className="card-icon text-center">
+                                        <i class="bi bi-chat-left-text"></i>
+                                    </div>
+                                    <p className="card-text"></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer className='bottom-0 w-100 mt-4'>
+                <div className="p-3 border-top border-3">
+                    <div className="d-flex justify-content-center m-1">
+                        <div className="align-self-center">
+                            <div className="top-icon px-2">
+                                <i class="bi bi-gear"></i>
+                            </div>
+                        </div>
+                        <div className="top-name align-self-center fs-2">
+                            <a className="navbar-brand" href="#">CSS Generater</a>
+                        </div>
+                    </div>
+                    <p className='text-center text-white'>&copy; Team Name 2023</p>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Home
